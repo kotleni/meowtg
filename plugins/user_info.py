bff66a44210d0fa640c9244b8f3c885bad7a8dac
kotleni <yavarenikya@gmail.com>
2023-11-20 01:26:17 +0200

    added .user command

---
+++ plugins/user_info.py
@@ -0,0 +1,19 @@
+from plugin_base import PluginBase
+from telethon import TelegramClient, events, types
+from telethon.tl.functions.messages import SendReactionRequest
+import subprocess
+    
+class UserInfo(PluginBase):
+    description = "User info by .user command"
+    enabled = True
+
+    def __init__(self, api) -> None:
+        super().__init__(api)
+        
+    async def on_command(self, event, args) -> str:
+        if args[0] == "user":
+            output = f"{event.chat.first_name} ({event.chat.username})\n"
+            output += f"{event.chat.id}\n"
+            output += f"{event.chat.phone}"
+
+            return output
