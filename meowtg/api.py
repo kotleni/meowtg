54618333fb3685d643ad383fdfe8474a4ad0d9c6
Viktor Varenik <yavarenikya@gmail.com>
2024-08-21 21:11:46 +0300

    chore: refactoring api.py

--- meowtg/api.py
+++ meowtg/api.py
@@ -1,16 +1,22 @@
+from plugins_loader import PluginsLoader
+
+from telethon import TelegramClient
+from telethon.types import User
+from logging import Logger
+
 class API:
-    client = None
-    logger = None
-    plugins_loader = None
+    client: TelegramClient = None
+    logger: Logger = None
+    plugins_loader: PluginsLoader = None
 
-    me = None
+    me: User = None
 
-    def __init__(self, client, logger, me):
+    def __init__(self, client: TelegramClient, logger: Logger, me: User):
         self.client = client
         self.logger = logger
         self.me = me
 
-    def register_plugins_loader(self, plugins_loader):
+    def register_plugins_loader(self, plugins_loader: PluginsLoader):
         self.plugins_loader = plugins_loader
 
     def get_my_id(self):
