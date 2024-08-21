3a569b764b188b7d458496aa075820b0fac5b079
Viktor Varenik <yavarenikya@gmail.com>
2024-08-21 22:32:09 +0300

    chore: bump version to v0.1.1-beta

--- meowtg/const.py
+++ meowtg/const.py
@@ -1,4 +1,4 @@
 LOGGER_FORMATTER = '(%(levelname)s | %(asctime)s) -> %(message)s'
 REPO = 'https://github.com/kotleni/meowtg/'
 PLUGINS_FOLDER_PATH = 'plugins/'
-VERSION_NAME = 'v0.1.0-beta'
+VERSION_NAME = 'v0.1.1-beta'
