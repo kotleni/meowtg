6ce45a61bfbf840d9c95252a03ef4102379b042f
Viktor Varenik <yavarenikya@gmail.com>
2024-08-21 21:31:45 +0300

    feat: default pkg plugin integrated to core

--- meowtg/main.py
+++ meowtg/main.py
@@ -86,6 +86,10 @@ async def main():
     api.register_plugins_loader(plugins_loader)
 
     logger.info("Loading plugins...")
+    
+    # Force load pkg as plugins
+    await plugins_loader.load_plugin('../meowtg/pkg.py')
+    
     await plugins_loader.load_plugins()
     await client.run_until_disconnected()
 
