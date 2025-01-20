7d9f42ae8c7233503ab323547691fcfe8de49e03
Viktor Varenik <yavarenikya@gmail.com>
2025-01-20 15:46:23 +0200

    chore: add CommandProcessor logging

--- meowtg/main.ts
+++ meowtg/main.ts
@@ -27,10 +27,14 @@ class CommandsProcessor {
     register(name: string, description: string, callback: () => void) {
         const command = { name: name, description: description, callback: callback };
         this.registered_commands.push(command);
+
+        console.log(`Registered command: ${command.name}`);
     }
 
     unregister(name: string): void {
         this.registered_commands = this.registered_commands.filter(command => command.name === name);
+
+        console.log(`Unregistered command: ${this.registered_commands.length}`);
     }
 
     find_command(name: string): Command {
