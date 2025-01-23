e33012a58d8ca2a28c8c21b796f5d6d375d46f68
Viktor Varenik <yavarenikya@gmail.com>
2025-01-23 17:21:49 +0200

    chore: remove legacy source files

--- legacy/meowtg/result_codes.py
+++
@@ -1,8 +0,0 @@
-# Result codes can be used as prefix for return'ed value.
-# Ak: return COMMAND_OK_MESSAGE_REPLY + 'OK!'
-
-"""Just remove message"""
-COMMAND_OK_MESSAGE_REMOVE = "$OK_MESSAGE_REMOVED"
-
-"""Use reply instead of editing message"""
-COMMAND_OK_MESSAGE_REPLY = "$COMMAND_OK_MESSAGE_REPLY"
